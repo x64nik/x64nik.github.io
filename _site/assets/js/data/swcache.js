9bb5634b49e30d83b9c14b7a96f5ea54711f1a3a
x64nik <rushidarunte123@gmail.com>
2023-03-04 13:15:12 +0530

    added new things

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -11,34 +11,18 @@ const resource = [
     '/404.html',
 
     
-        '/categories/',
+        '/tabs/categories.html',
     
-        '/tags/',
+        '/tabs/tags.html',
     
-        '/archives/',
+        '/tabs/archives.html',
     
-        '/about/',
+        '/tabs/about.html',
     
 
     /* --- Favicons & compressed JS --- */
     
     
-        '/assets/img/favicons/android-chrome-192x192.png',
-        '/assets/img/favicons/android-chrome-512x512.png',
-        '/assets/img/favicons/apple-touch-icon.png',
-        '/assets/img/favicons/favicon-16x16.png',
-        '/assets/img/favicons/favicon-32x32.png',
-        '/assets/img/favicons/favicon.ico',
-        '/assets/img/favicons/mstile-150x150.png',
-        '/assets/img/favicons/site.webmanifest',
-        '/assets/img/favicons/x64nik.png',
-        '/assets/js/dist/categories.min.js',
-        '/assets/js/dist/commons.min.js',
-        '/assets/js/dist/home.min.js',
-        '/assets/js/dist/misc.min.js',
-        '/assets/js/dist/page.min.js',
-        '/assets/js/dist/post.min.js',
-        '/assets/js/dist/pvreport.min.js'
 ];
 
 /* The request url with below domain will be cached */
